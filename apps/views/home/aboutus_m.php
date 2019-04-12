cb73fd02abafb1bb6f0e03c5a36f89ecd853ad60
Web Dev <app.dev0528@gmail.com>
2019-04-12 04:39:32 +0300

    view-home-aboutus

---
+++ apps/views/home/aboutus_m.php
@@ -0,0 +1,11 @@
+<div align="center" >
+<h3 style="color: #1141cc;margin-top: 30px;">Find us here</h3>
+					<div><i class="icon-home "></i>  Address:   <small>Melboume, Victoria </small>                       <small> Australia 3000</small></div>
+				
+				  	<div><i class="icon-headphones"></i>  Mobile:    <small>+40-745.036.789,</small>
+						                        	 <small>+40-745.675.631<br></small></div>
+						  
+				  <div style="margin-bottom: 30px;" ><i class="icon-envelope"></i>  Email:    <small style="color: #1141cc;">     hello@hirevee.com<br></small></div>
+				  
+
+</div>
